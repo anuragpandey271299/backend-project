2bdb2def9d99999fc2ebd525b014149e320ebcb8
anuragpandey271299 <anurag.info27@gmail.com>
2023-12-06 03:05:20 +0530

    connected to database

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const dotenv=require('dotenv').config()
 
 
 const app=express()
@@ -10,5 +11,8 @@ app.get('/',(req,res)=>{
 })
 
 app.listen(3000,()=>{
+    mongoose.connect(process.env.MONGODB_URL)
+    .then(()=>console.log('DB connected'))
+    .catch((error)=>console.log(error))
     console.log('Server running on http://localhost:3000')
 })
